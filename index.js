477108e43c75318731094aad6d8dea8cc1d5fe98
ndrbr <andrey.burdak@gmail.com>
2023-04-03 21:40:04 +0700

    Added CSV parsing

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
-const parse = require("csv-parse");
-console.log("imported");
+const { parse } = require("csv-parse");
+const fs = require("fs");
+
+// import { parse } from "csv-parse";
+// import { createReadStream } from "fs";
+// import * as fs from "fs";
+
+const habitablePlanets = [];
+
+function isHabitablePlant(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitablePlant(data)) habitablePlanets.push(data);
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(
+      habitablePlanets.map((planet) => {
+        return planet["kepler_name"];
+      })
+    );
+    console.log(`${habitablePlanets.length} habitable planets found!`);
+  });
